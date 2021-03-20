661b79770ae14858cbffd24b2afea3420840ea72
Henry Eason <henryeason50@gmail.com>
2021-03-20 14:22:39 +0000

    Fix favourites

--- api/src/endpoints/account/addFavourite.ts
+++ api/src/endpoints/account/addFavourite.ts
@@ -1,4 +1,8 @@
-import { error, query, send } from "../utils";
+import {
+  error,
+  query,
+  send
+} from "../utils";
 
 const SQL_ADD = `
   INSERT INTO Favourites (
@@ -7,15 +11,29 @@ const SQL_ADD = `
   ) VALUES (?, ?);
 `;
 
+const SQL_FAVOURITE_EXISTS = `
+  SELECT EXISTS (
+    SELECT
+      \`id\`
+    FROM Favourites
+    WHERE \`user\` = ?
+    AND \`recipe\` = ?
+  ) AS \`exists\`;
+`;
+
 export default async function addFavourite(req, res) {
   let recipe = parseInt(req.body.recipe);
 
   if (!recipe || recipe <= 0)
     return error(res);
 
-  let result = await query(res, SQL_ADD, [recipe, req.user]);
+  let existsQuery = await query(res, SQL_FAVOURITE_EXISTS, [req.user, recipe]);
+  if (existsQuery[0]["exists"] == 1)
+    return error(res);
+
+  let result = await query(res, SQL_ADD, [req.user, recipe]);
 
   send(res, {
-    favourite: result[0]["insertId"]
+    favourite: result["insertId"]
   });
 }
